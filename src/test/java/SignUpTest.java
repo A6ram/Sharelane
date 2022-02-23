a124cac937b388339e9ba3599a9d158120d1579c
Илья <illia.a6ram@gmail.com>
2022-02-23 11:16:07 +0300

    A6ram

--- src/test/java/SignUpTest.java
+++ src/test/java/SignUpTest.java
@@ -15,33 +15,42 @@ public class SignUpTest {
     }
 
     @Test
-    public void zipCode(){
-        /*
-        1.Открыть браузер
-        2. Перейти по ссылке https://www.sharelane.com/cgi-bin/register.py
-        3. Вводим 5 цифр, например 12345
-        4.Нажать Continue
-        5.Проверить, что кнопка Register видна
-        6.Вводим поле First name, например Illia
-        7.Вводим поле Last name, например Abramovich
-        8.Вводим поле Email, например i.a6ram@gmail.com
-        9.Вводим поле Password, например pipika
-        10. Вводим поле Confirm password, например pipika
-        11. Закрыть браузер
-         */
+    public void zipCode() {
 
         driver.get("https://www.sharelane.com/cgi-bin/register.py");
         driver.findElement(By.name("zip_code")).sendKeys("12345");
         driver.findElement(By.cssSelector("[value=Continue]")).click();
-        boolean isDispLayed =driver.findElement(By.cssSelector("[value=Register]")).isDisplayed();
+        boolean isDispLayed = driver.findElement(By.cssSelector("[value=Register]")).isDisplayed();
         Assert.assertTrue(isDispLayed);
 
-            driver.findElement(By.name("first_name")).sendKeys("Illia");
-            driver.findElement(By.name("last_name")).sendKeys("Abramovich");
-            driver.findElement(By.name("email")).sendKeys("i.a6ram@gmail.com");
-            driver.findElement(By.name("password1")).sendKeys("pipika");
-            driver.findElement(By.name("password2")).sendKeys("pipika");
-            driver.findElement(By.cssSelector("[value=Register")).click();
+    }
+    @Test
+    public void zipCodePozitive(){
+        driver.get("https://www.sharelane.com/cgi-bin/register.py?page=1&zip_code=12345");
+        driver.findElement(By.name("first_name")).sendKeys("Illia");
+        driver.findElement(By.name("last_name")).sendKeys("Abramovich");
+        driver.findElement(By.name("email")).sendKeys("i.a6ram@gmail.com");
+        driver.findElement(By.name("password1")).sendKeys("pipika");
+        driver.findElement(By.name("password2")).sendKeys("pipika");
+        driver.findElement(By.cssSelector("[value=Register")).click();
+        boolean isDisplayed= driver.findElement(By.className("confirmation_message")).isDisplayed();
+        Assert.assertTrue(isDisplayed);
+    }
+    @Test
+    public void zipCodeNegative1() {
+        driver.get("https://www.sharelane.com/cgi-bin/register.py");
+        driver.findElement(By.name("zip_code")).sendKeys("1234");
+        driver.findElement(By.cssSelector("[value=Continue]")).click();
+        boolean isDispLayed = driver.findElement(By.className("error_message")).isDisplayed();
+        Assert.assertTrue(isDispLayed);
+    }
+    @Test
+    public void zipCodeNegative2(){
+        driver.get("https://www.sharelane.com/cgi-bin/register.py");
+        driver.findElement(By.name("zip_code")).sendKeys("123456");
+        driver.findElement(By.cssSelector("[value=Continue]")).click();
+        boolean isDispLayed = driver.findElement(By.className("error_message")).isDisplayed();
+        Assert.assertTrue(isDispLayed);
     }
     @AfterMethod(alwaysRun = true)
     public void tearDoun(){
